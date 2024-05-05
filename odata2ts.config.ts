e4bc7a2ab6ac0bbf190b87d727b7b8d912f22a88
texttechne <gedankengut@gmx.de>
2024-05-05 13:29:27 +0200

    feat: setup repo

---
+++ odata2ts.config.ts
@@ -0,0 +1,45 @@
+import { ConfigFileOptions, EmitModes, Modes, TypeModel } from "@odata2ts/odata2ts";
+
+const config: ConfigFileOptions = {
+  debug: false,
+  mode: Modes.service,
+  emitMode: EmitModes.ts,
+  prettier: true,
+  services: {
+    dynamics: {
+      serviceName: "Dynamics",
+      mode: Modes.service,
+      source: "odata/dynamics.xml",
+      output: "build/dynamics",
+      allowRenaming: true,
+      bundledFileGeneration: false,
+      byTypeAndName: [
+        {
+          type: TypeModel.EntityType,
+          name: "solution",
+          mappedName: "SolutionEntity",
+        },
+      ],
+    },
+    casePacer: {
+      emitMode: EmitModes.ts,
+      mode: Modes.service,
+      bundledFileGeneration: false,
+      serviceName: "CasePacer",
+      source: "odata/casePacer.xml",
+      output: "build/casePacer",
+    },
+    trippin: {
+      bundledFileGeneration: false,
+      source: "odata/trippin.xml",
+      output: "build/trippin",
+    },
+    trippinBi: {
+      bundledFileGeneration: false,
+      source: "odata/trippin-bidirectional.xml",
+      output: "build/trippinBi",
+    }
+  },
+};
+
+export default config;
